ec92731dd2e4b1ee7963a5438b8e6d9bd46ea7fd
megh2 <megh@stylabs.in>
2021-07-11 21:55:11 +0530

    init repo with crud for posts using graphql and mikroORM

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import { MikroORM } from "@mikro-orm/core";
+import dotenv from 'dotenv';
+import microConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+dotenv.config();
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em })
+  });
+
+  apolloServer.applyMiddleware({ app });
+  app.get('/', (_, res) => {
+    res.send('<h1>NOICE</h1>');
+  });
+  app.listen(process.env.PORT, () => {
+    console.log(`App running on port: ${process.env.PORT}`);
+  });
+}
+
+try {
+  main();
+} catch (e) {
+  console.error(e);
+}
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,16 @@
+import { MikroORM } from "@mikro-orm/core";
+import { Post } from "./entities/Post";
+import path from 'path';
+
+export default {
+  migrations: {
+    path: path.join(__dirname, './migrations'),
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+  },
+  dbName: process.env.DB_NAME ? process.env.DB_NAME : 'reddit-clone',
+  user: process.env.DB_USER ? process.env.DB_USER : 'megh',
+  password: process.env.DB_PASS ? process.env.DB_PASS : 'megh123',
+  type: 'postgresql',
+  debug: process.env.NODE_ENV !== 'production',
+  entities: [Post]
+} as Parameters<typeof MikroORM.init>[0];
